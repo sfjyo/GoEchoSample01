5a6144111c1e3631561e77f83c2f6fa037aeac8e
sfjyo <sfjyo.devel@gmail.com>
2019-09-16 11:27:05 +0900

    修正

--- server.go
+++ server.go
@@ -1,36 +1,15 @@
 package main
 
 import (
-	"html/template"
-	"io"
-	"net/http"
+  "fmt"
+  "net/http"
 )
 
-type TemplateRender struct {
-	templates *template.Template
+func handler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Hello, World")
 }
-
-func (t *TemplateRender) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	return t.templates.ExecuteTemplate(w, name, data)
-}
-
 
 func main() {
-	render := &TemplateRender {
-		templates: template.Must(template.ParseGlob("public/views/*.html")),
-	}
-	e := echo.New()
-	e.Renderer = render
-	e.Use(middleware.Logger())
-
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello")
-	})
-	// e.GET("/users/:name", func(c echo.Context) error {
-	// 	name := c.Param("name")
-	// 	return c.String(http.StatusOK, "Hello: " + name)
-	// })
-
-	e.Logger.Fatal(e.Start(":1323"))
+  http.HandleFunc("/", handler)
+  http.ListenAndServe(":8080", nil)
 }
-
